d80b8b8cee3722852d20c78618bf5b601f210315
annyhira414 <annyhira@gmail.com>
2022-06-17 22:16:00 +0600

    practice

---
+++ Annotations(TRY)/Decorators python_functions are objects/see_what_happend.py
@@ -0,0 +1,21 @@
+# def one(word='three'):
+#     return word.capitalize() + ' is the number'
+#
+#
+# print(one())
+#
+# two = one
+# '''
+# notice i don't use the parentheses : i am not calling function , just putting the function
+# '''
+# print(two())
+#
+# del one
+# try:
+#     print(one())
+# except NameError as e:
+#       print e
+#
+# print(two())
+#
+#
