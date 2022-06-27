6d39892c22bcbace1908949bfe53099a9c6a8f1b
annyhira414 <annyhira@gmail.com>
2022-06-27 23:34:16 +0600

    try solved Palindrome Number

---
+++ Problem/LeetCode/Palindrome Number 9/Palindrome Number.py
@@ -0,0 +1,42 @@
+# # class Solution:
+# def isPalindrome(self, x):
+#         sum = 0
+#         # x = 121
+#         temp = x
+#         # if x > 0 : return True
+#         # if x < 0 : return  False
+#         input('enter num', x)
+#         while temp != 0:
+#             r = temp % 100
+#             sum = sum + r
+#             temp = temp // 10
+#
+#         if x == sum:
+#             return True
+#         else:
+#             return False
+#
+#
+# # ans = Solution()
+# # x = (121)
+# # print(ans.isPalindrome())
+
+'''
+ aita hoyse but leet code jonno aita hoy nai
+
+n = int(input("enter your number :"))
+a = n
+rev = 0
+while n > 0:
+    r = n % 10
+    rev = rev * 10 + r
+    n = n // 10
+
+if a == rev:
+    print("ok")
+    #return False
+else:
+    print("no")
+
+  '''
+
